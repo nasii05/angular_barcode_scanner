0819ca6049a0d7e5405be2bcfa84a0fe1d0a4e6d
nasii05 <iamnasih@gmail.com>
2023-08-24 15:23:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'barcode';
+
+  value: string = "";
+  isError = false;
+
+  onError(error: any) {
+    console.error(error);
+    this.isError = true;
+  }
+  
 }
